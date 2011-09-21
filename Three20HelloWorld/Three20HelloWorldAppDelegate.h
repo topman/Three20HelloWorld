cbcb513b382b3e7c50f4e413bb20e7253aa9b942
Tower Joo <zhutao.iscas@gmail.com>
2011-09-21 18:08:37 +0800

    Initial Commit

---
+++ Three20HelloWorld/Three20HelloWorldAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Three20HelloWorldAppDelegate.h
+//  Three20HelloWorld
+//
+//  Created by tower on 11-9-21.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Three20HelloWorldAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
